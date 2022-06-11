1511044fd68d3c2f2c9b6170b18a7ff4c4f95192
jucelia xavier da silva <juceliaxavier29@gmail.com>
2022-06-10 22:45:22 -0300

    inicio do projeto

---
+++ sistema-bancario/src/index.ts
@@ -0,0 +1,28 @@
+console.log("Hello, world!")
+
+import express from 'express'
+import { Request, Response } from 'express'
+
+import { AddressInfo } from "net";
+
+const app = express();
+
+app.use(express.json());
+
+
+
+app.get("/ping", (req, res) => {          
+    res.send("Pong! 🏓")
+})
+
+
+
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+      console.error(`Failure upon starting server.`);
+    }
+  });; 
