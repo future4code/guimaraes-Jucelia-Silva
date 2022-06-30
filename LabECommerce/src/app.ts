104496109988858a6784ec47e5fbd1245279ddb1
jucelia xavier da silva <juceliaxavier29@gmail.com>
2022-06-30 20:22:23 -0300

    labecommerce

---
+++ LabECommerce/src/app.ts
@@ -0,0 +1,11 @@
+import express, {Express} from "express";
+import cors from "cors"
+
+const app : Express = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003,()=>{ console.log("Olá") })
+
+export default app;
