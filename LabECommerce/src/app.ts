1b0958ed9aaa19f129465a9c2699c6622ca69b27
jucelia xavier da silva <juceliaxavier29@gmail.com>
2022-07-10 20:53:41 -0300

    atualização

--- LabECommerce/src/app.ts
+++ LabECommerce/src/app.ts
@@ -1,11 +1,17 @@
-import express, {Express} from "express";
+import express from "express"
 import cors from "cors"
+import { AddressInfo } from "net"
 
-const app : Express = express()
+export const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.listen(3003,()=>{ console.log("Olá") })
-
-export default app;
+const server = app.listen(process.env.PORT || 3003, () => {
+   if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`Server is running in http://localhost:${address.port}`);
+   } else {
+      console.error(`Failure upon starting server.`);
+   }
+})
